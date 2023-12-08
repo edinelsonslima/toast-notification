9563f3619fe5ea91603b119465483ebd0acbedd9
edinelsonslima <edinelsonlima86@gmail.com>
2023-12-08 13:30:07 -0300

    added js doc

--- src/helpers/toast.helper.ts
+++ src/helpers/toast.helper.ts
@@ -1,30 +1,41 @@
-import { ReactNode } from "react";
 import EventManager from "../services/event-manager.service";
 import { toastType } from "./toast-type.helpers";
 import { toastContentSanitize } from "./toast-content-sanitize.helpers";
-
-export type IReactNode = Omit<ReactNode, "object"> | undefined;
-
-export interface IToastWithoutType {
-  content: IReactNode;
-  duration?: number;
-}
-
-export interface IToast extends IToastWithoutType {
-  type: "success" | "info" | "warn" | "error" | "ghost";
-}
+import { IToast } from "../@types";
 
 export const toastEventManager = new EventManager<{ "add-toast": IToast }>();
 
+/**
+ * @param {object} { duration, content, type }
+ */
 function toast({ duration, content, type }: IToast) {
   content = toastContentSanitize(content);
   toastEventManager.emit("add-toast", { content, duration, type });
 }
-
+/**
+ * @param {object} { duration, content }
+ * @param {ReactNode} ReactNode ReactElement | string | number | Iterable<ReactNode> | ReactPortal | boolean | null | undefined
+ */
 toast.error = toastType("error");
+/**
+ * @param {object} { duration, content }
+ * @param {ReactNode} ReactNode ReactElement | string | number | Iterable<ReactNode> | ReactPortal | boolean | null | undefined
+ */
 toast.success = toastType("success");
+/**
+ * @param {object} { duration, content }
+ * @param {ReactNode} ReactNode ReactElement | string | number | Iterable<ReactNode> | ReactPortal | boolean | null | undefined
+ */
 toast.warn = toastType("warn");
+/**
+ * @param {object} { duration, content }
+ * @param {ReactNode} ReactNode ReactElement | string | number | Iterable<ReactNode> | ReactPortal | boolean | null | undefined
+ */
 toast.info = toastType("info");
+/**
+ * @param {object} { duration, content }
+ * @param {ReactNode} ReactNode ReactElement | string | number | Iterable<ReactNode> | ReactPortal | boolean | null | undefined
+ */
 toast.ghost = toastType("ghost");
 
 export { toast };
