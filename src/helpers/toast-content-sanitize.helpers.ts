774c1f930c017c21bc6b9d8d57879167f712580e
edinelsonslima <edinelsonlima86@gmail.com>
2023-12-08 00:14:56 -0300

    sanitize content for show in toast

---
+++ src/helpers/toast-content-sanitize.helpers.ts
@@ -0,0 +1,14 @@
+import { isValidElement } from "react";
+import { IReactNode } from "./toast.helper";
+
+export function toastContentSanitize(content?: IReactNode): IReactNode {
+  if (!content) return;
+
+  if (isValidElement(content)) {
+    return content;
+  }
+
+  return !(typeof content === "object") || Array.isArray(content)
+    ? content
+    : JSON.stringify(content);
+}
