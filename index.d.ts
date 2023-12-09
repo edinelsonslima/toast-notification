861bd68d51849445173710d98e5752100f14f05d
edinelsonslima <edinelsonlima86@gmail.com>
2023-12-08 21:25:16 -0300

    remove files

---
+++ index.d.ts
@@ -0,0 +1,81 @@
+import { CSSProperties } from 'react';
+import { JSX as JSX_2 } from 'react/jsx-runtime';
+import { ReactNode } from 'react';
+
+export declare type IClassNames = IClassNamesAnimations | CSSProperties;
+
+export declare type IClassNamesAnimations = HTMLSpanElement["className"] | undefined;
+
+export declare type IPositions = "right-top" | "right-center" | "right-bottom" | "center-top" | "center-center" | "center-bottom" | "left-top" | "left-center" | "left-bottom";
+
+export declare type IReactNode = Omit<ReactNode, "object"> | undefined;
+
+export declare interface IToast extends IToastWithoutType {
+    type: "success" | "info" | "warn" | "error" | "ghost";
+}
+
+declare type IToastContainerClassNames = {
+    [position in IPositions | "*"]?: IClassNames;
+};
+
+export declare interface IToastContainerProps {
+    /**
+     * @description define default duration in screen for all toast messages
+     * @type number
+     * @default undefined
+     */
+    defaultDuration?: number;
+    /**
+     * @description define position in screen for all toast messages
+     * @type IPositions
+     * @default 'right-top'
+     */
+    position?: IPositions;
+    /**
+     * @description define styles for toast
+     * @default undefined
+     */
+    classNames?: {
+        /**
+         * @description change default animation screen out of the all toasts
+         * @default undefined
+         */
+        animationUnmount?: IClassNamesAnimations;
+        /**
+         * @description change default position style in screen of the all toasts
+         * @default undefined
+         */
+        toastContainer?: IToastContainerClassNames;
+        /**
+         * @description change default style of the all toasts
+         * @default undefined
+         */
+        toastMessages?: IToastMessageClassNames;
+    };
+}
+
+declare type IToastMessageClassNames = {
+    [type in IToast["type"] | "*"]?: IClassNames;
+};
+
+declare interface IToastWithoutType {
+    content: IReactNode;
+    duration?: number;
+}
+
+/**
+ * @param {object} { duration, content, type }
+ */
+export declare function toast({ duration, content, type }: IToast): void;
+
+export declare namespace toast {
+    var error: (data: IReactNode | IToastWithoutType) => void;
+    var success: (data: IReactNode | IToastWithoutType) => void;
+    var warn: (data: IReactNode | IToastWithoutType) => void;
+    var info: (data: IReactNode | IToastWithoutType) => void;
+    var ghost: (data: IReactNode | IToastWithoutType) => void;
+}
+
+export declare function ToastContainer({ classNames, defaultDuration, position, }: IToastContainerProps): JSX_2.Element;
+
+export { }
--- src/helpers/get-custom-css.helpers.ts
+++
@@ -1,9 +0,0 @@
-export function getStyle(value?: string | object) {
-  if (!value || typeof value !== "object") return;
-  return value;
-}
-
-export function getClassName(value?: string | object) {
-  if (!value || typeof value !== "string") return "";
-  return value;
-}
